a3d75a1b3e1c72d054832aff59fa48457aae8e4e
NUToshiSakamoto <nutoshi@nutmail.nuts>
2016-04-14 22:10:20 +0100

    Minor fixes for Qt wallet compilation

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -17,6 +17,7 @@
 #include <QDateTime>
 #include <QDialogButtonBox>
 #include <QFlags>
+#include <QHeaderView>
 #include <QIcon>
 #include <QString>
 #include <QTreeWidget>
@@ -96,7 +97,7 @@ CoinControlDialog::CoinControlDialog(QWidget *parent) :
     connect(ui->treeWidget, SIGNAL(itemChanged( QTreeWidgetItem*, int)), this, SLOT(viewItemChanged( QTreeWidgetItem*, int)));
     
     // click on header
-    ui->treeWidget->header()->setClickable(true);
+    ui->treeWidget->header()->setSectionsClickable(true);
     connect(ui->treeWidget->header(), SIGNAL(sectionClicked(int)), this, SLOT(headerSectionClicked(int)));
     
     // ok button
